3fb5021b6f932b34fe06f9a9ba2a591681e63143
LelandLong <lelandlong@me.com>
2022-06-04 18:30:01 -0400

    app using hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scroll from '../components/Scroll';
 import './App.css';
 import CardList from '../components/CardList';
-//import { robots } from './robots';
 import SearchBox from '../components/SearchBox';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: ''
-    };
-    // console.log('1 constructor');
-  };
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  componentDidMount() {
-    // this.setState({ robots: robots });
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => {
       // console.log('2 didMount; users: ', users);
-      this.setState({ robots: users });
+      setRobots(users);
     });
-  }
+  }, []);
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value});
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
     //console.log(filteredRobots);
-  }
+  };
 
-  render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-    // console.log('3 render');
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+  console.log('App robots:' + JSON.stringify(robots) + ' ; searchField:' + searchField);
 
-    if (!robots.length) {  // robots.length === 0
-      return <h1 className='tc f1'>Loading...</h1>
-    } else {
-      return (
-        <div className='tc'>
-          <h1 className='f1'>RobotFriends</h1>
-          <React.StrictMode>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-              <CardList robots={filteredRobots} />
-            </Scroll>
-          </React.StrictMode>
-        </div>
-      );
-    };
+  if (!robots.length) {  // robots.length === 0
+    return <h1 className='tc f1'>Loading...</h1>
+  } else {
+    return (
+      <div className='tc'>
+        <h1 className='f1'>RobotFriends</h1>
+        <React.StrictMode>
+          <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </React.StrictMode>
+      </div>
+    );
   };
 }
 
